6f3f16fcc1ec565e19d05ee60463585f37e83a16
Zoë <zoe@guido-it.nl>
2023-12-01 16:36:10 +0100

    Day one done! 2*

---
+++ day1/part1.py
@@ -0,0 +1,15 @@
+import re
+
+f = open("dag1/input.txt")
+INPUT = f.readlines()
+
+total = 0
+
+for i in INPUT:
+    numbers = re.findall("([\d]+)", i)
+    print(numbers)
+    
+    print(numbers[0][0] + numbers[-1:][0][-1:])
+    total += int(numbers[0][0] + numbers[-1:][0][-1:])
+
+print("Total: ", total)
---
+++ day1/part2.py
@@ -0,0 +1,76 @@
+f = open("dag1/input.txt")
+INPUT = f.readlines()
+NUMBERS = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+]
+
+TXTNUMBERS = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+]
+
+total = 0
+
+for i in INPUT:
+    print(i.split("\n")[0])
+
+    firstindex = len(i)
+    firstnumber = 0
+    lastindex = 0
+    lastnumber = 0
+
+    for a in NUMBERS:
+        if (i.find(a) > -1):
+            if i.find(a) < firstindex:
+                firstindex = i.find(a)
+                firstnumber = a
+            
+            if i.rfind(a) > lastindex:
+                lastindex = i.rfind(a)
+                lastnumber = a
+
+    print(firstnumber, lastnumber)
+
+    try:
+        firstnumber = int(firstnumber)
+    except:
+        firstnumber = TXTNUMBERS.index(firstnumber)
+
+    try:
+        lastnumber = int(lastnumber)
+    except:
+        lastnumber = TXTNUMBERS.index(lastnumber)
+
+    if lastnumber == 0:
+        lastnumber = firstnumber
+
+    print(int(str(firstnumber) + str(lastnumber)))
+
+    total += int(str(firstnumber) + str(lastnumber))
+
+print("Total: ", total)
